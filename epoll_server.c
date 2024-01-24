dfc8e99f87613cc7de8e4f673e90e1defb6266f0
Samuel Skean <sskean2@uic.edu>
2024-01-24 01:40:12 -0600

    WIP. Making stuff work slowly. Still very much looking at Eriksson's code but it's getting to the point where the problems are different and I'm trying to look more at his epoll code (so I can learn those concepts) and less at his threaded echo server, because I think I have a shot at solving that problem myself.

--- epoll_server.c
+++ epoll_server.c
@@ -1,13 +1,78 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <netinet/in.h> // TODO: What's the difference between this and sys/socket.h,
                         // and why should I care?
+#include <sys/epoll.h>
+
+#define MAX_FDS 20 // STRETCH TODO: Try increasing this.
+
 
 void unrecoverable_error(char* error_string) {
-    perror(error_string);
+    const char* fatal = "FATAL: ";
+    char output[100]; // I happen to know that all my error messages are smaller than this.
+    strcpy(output, fatal);
+    strcat(output, error_string);
+    perror(output);
     exit(EXIT_FAILURE);
 }
+
+// This code is copied verbatim from the epoll_server_example code,
+// and I don't really care to understand it at the moment. Why do we have
+// to check the flags first?
+void set_socket_non_blocking(int socket_fd) {
+    int flags = fcntl(socket_fd, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL");
+        exit(EXIT_FAILURE);
+    }
+    if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl F_SETFL O_NONBLOCK");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// This code is copied verbatim from the example server.c code.
+void broadcast_message(char *message, int sender_sd) {
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        int sd = client_sockets[i];
+        if (sd > 0 && sd != sender_sd) {
+            send(sd, message, strlen(message), 0);
+        }
+    }
+}
+
+struct {
+    char* buffer;
+    size_t buffer_size;
+    size_t used_bytes;
+    int messages_sent;
+} client_socket_states[MAX_FDS]; // This array is indexed into with file descriptors. Thank god they start numbering pretty low!
+
+// Returns 1 when the client socket should stay open, 0 when the client socket should be closed.
+int handle_client(int socket) {
+    while (1) {
+        // TODO: Make sure there's enough room to at least read *some* more. I'm curious how his original code handles the fact that a client's message could be arbitrarily long, but I'll just use realloc, I think.
+        int num_bytes_read = read(socket, client_socket_states[socket].buffer +
+                client_socket_states[socket].used_bytes, client_socket_states[socket].buffer_size - client_socket_states.used_bytes);
+        if (num_read_bytes == -1) { // This return value means some "error" occurred.
+            switch(errno) {
+                case EAGAIN: break; // There may be more to read later.
+                default:
+                    printf("Error on socket %d\n", socket); // An actual error occurred.
+            }
+            break;
+        }
+        if (num_read_bytes == 0) {
+            return 0;
+        }
+
+
+
+
 
 int main(int argc, char* argv[]) {
     int c; // Used for parsing options
@@ -40,23 +105,23 @@ int main(int argc, char* argv[]) {
     // Done parsing options. Real server logic begins here:
     //
 
-    int server_socket, new_socket;
-        // server_socket: socket that listens and accepts
-        // new_socket: created for each client, maintains connection
-    int* new_sock_ptr;
-    struct sockaddr_in server, client;
+    int listen_fd, conn_fd;
+        // listen_fd: socket that listens and accepts
+        // conn_fd: created for each client, maintains connection
+    // int* new_sock_ptr;
+    struct sockaddr_in server; // This "_in" has nothing at all to do with the address being an "in" address (whatever that would mean), it just means that the address is an INternet address. I really dislike APIs that can't be bothered to speak English.
 
     // Create socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0); 
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0); 
         // domain = AF_INET: IPv4
         // type = SOCK_STREAM: two-way stream
         // protocol = 0: use the default protocol associated with this domain 
-    if (server_socket == -1) {
+    if (listen_fd == -1) {
         unrecoverable_error("Could not create socket");
     }
 
     int reuseaddr_opt = 1; // we want to set the reuseaddr option to true
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_opt, sizeof(reuseaddr_opt))) {
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_opt, sizeof(reuseaddr_opt))) {
         // SOL_SOCKET - SOcketLevel_SOCKET: set options at the socket API level 
         //      (presumably the highest level allowed)
         // SO_REUSEADDR: the actual option we're setting, allow addresses to be reused quickly when the program quits. https://www.baeldung.com/linux/socket-options-difference - tries to explain it, but explicitly refers to BSD over Linux and is confusing (and sort of conflicting with some other info online, that says that at most one socket can every have a given address-port pair - this program might conflict with that though, with multiple clients ending up connected on the same port).
@@ -70,20 +135,51 @@ int main(int argc, char* argv[]) {
     server.sin_port = htons(port); // HostTONetwork byte order (I assume the s stands for socket)
 
     // Bind the port that listens:
-    if (bind(server_socket, (struct sockaddr*) &server, sizeof(server)) < 0) {
+    if (bind(listen_fd, (struct sockaddr*) &server, sizeof(server)) < 0) {
         unrecoverable_error("bind failed");
     }
 
 
-    listen(server_socket, 3); // backlog = 3: buffer of 3 connections held by
-                              // the kernel for us
+    if (listen(listen_fd, 3) < 0) { // backlog = 3: buffer of 3 connections held by
+                                    // the kernel for us
+        unrecoverable_error("listen failed");
+    }
 
     // Accept incoming connections
     puts("Waiting for incoming connections...");
-    socklen_t socket_length = sizeof(struct sockaddr_in);
 
-    while ((new_socket = accept(server_socket, (struct sockaddr*) &client, &socket_length))) {
-        puts("Accepting connection.");
+    const int MAXEVENTS = 1000;
+    struct epoll_event event; // event to use to add more file descriptors to wait on?
+    struct epoll_event* events; // Pointer to array of events representing client events to wait on
+    events = calloc(MAXEVENTS, sizeof(event));
+
+    int epoll_file_descriptor = epoll_create1(0); // Used purely with calls to the epoll interface
+    event.data.fd = listen_fd;
+    event.events = EPOLLIN;
+    if (epoll_ctl(epoll_file_descriptor, EPOLL_CTL_ADD, listen_fd, &event)) {
+        unrecoverable_error("epoll_ctl failed on adding listen_fd");
     }
 
+    while (1) { // We can no longer simply block on accept in the loop, since if we block, the server does no work
+        int num_ready_fds = epoll_wait(epoll_file_descriptor, events, MAXEVENTS, -1); // last argument is timeout, which we choose not to specify by passing -1. epoll_wait will block indefinitely (until there are ready events)
+        for (int i = 0; i < num_ready_fds; i++) {
+            if (events[i].data.fd == listen_fd) {
+                conn_fd = accept(listen_fd, NULL, NULL); // We don't care about the address of the client, so we set the address and addrlen to NULL
+                    // *now* we can accept, we know it won't delay us since we know there's a connection to accept
+                if (conn_fd < 0) {
+                    perror("Failed to accept incoming connection");
+                    continue;
+                }
+
+                event.data.fd = conn_fd;
+                event.events = EPOLLIN | EPOLLET; // The client connections are edge-triggered.
+                if (epoll_ctl(epoll_file_descriptor, EPOLL_CTL_ADD, conn_fd, &event) != 0) {
+                    perror("epoll_ctl failed on adding client socket");
+                }
+                set_socket_non_blocking(conn_fd);
+            } else { // We are dealing with a client connection.
+                puts("Received something from a client, ready to read it.\n");
+            }
+        }
+    }
 }
